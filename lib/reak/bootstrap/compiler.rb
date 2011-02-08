b4d6028908b9b2d02364ba21da43729254f3d737
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-02-08 18:15:20 +0100

    Compiler stages for parsing

--- lib/reak/bootstrap/compiler.rb
+++ lib/reak/bootstrap/compiler.rb
@@ -1,5 +1,47 @@
 module Reak
   class Compiler < Rubinius::Compiler
+    class Parser < Rubinius::Compiler::Parser
+      def initialize(compiler, last)
+        super
+        @processor = Reak::Parser
+      end
+
+      def create
+        # TODO: we totally ignore @transforms
+        @parser = @processor.new(@file, @line)
+        @parser
+      end
+    end
+
+    class FileParser < Parser
+      stage :reak_file
+      next_stage Generator
+
+      def input(file, line = 1)
+        @file = file
+        @line = line
+      end
+
+      def parse
+        create.parse_file
+      end
+    end
+
+    class StringParser < Parser
+      stage :reak_string
+      next_stage Generator
+
+      def input(string, name = "(eval)", line = 1)
+        @input = string
+        @file = name
+        @line = line
+      end
+
+      def parse
+        create.parse_string(@input)
+      end
+    end
+
     def self.compiled_name(file)
       file + (file.suffix?(".st") ? "c"  : ".compiled.stc")
     end
