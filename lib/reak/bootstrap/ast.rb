a4e7f5f43bae6ccda4aeed8b682387f824a8dedf
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-02-13 20:48:51 +0100

    initial rules for grammar

--- lib/reak/bootstrap/ast.rb
+++ lib/reak/bootstrap/ast.rb
@@ -5,9 +5,16 @@ module Reak
 
     def self.grammar_for(dialect, g = nil)
       g ||= KPeg::Grammar.new
+      rules_for(dialect, g)
       Expression.grammar_for(dialect, g)
       g.root = g.expression
       g
+    end
+
+    def self.rules_for(dialect, g)
+      g.sp = g.kleene g.any(" ", "\n")
+      g.sig_sp = g.many g.any(" ", "\n")
+      g.identifier = /[a-zA-Z][a-zA-Z0-9_]*/
     end
   end
 end
