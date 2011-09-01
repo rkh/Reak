c51fb0d21b1c4d391681ad673e80fb77668f5362
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-08-31 21:04:54 -0600

    load Alpha.st on `require 'reak'`

--- lib/reak.rb
+++ lib/reak.rb
@@ -1,10 +1,16 @@
 require 'kpeg'
 
 module Reak
+  # set up ruby stuff
   require 'reak/version'
   require 'reak/signature'
   require 'reak/bootstrap/tools'
   require 'reak/bootstrap/parser'
   require 'reak/bootstrap/ast'
   require 'reak/bootstrap/compiler'
+  require 'reak/bootstrap/codeloader'
+
+  # kick off smalltalk runtime
+  CodeLoader.load_paths << File.expand_path('../reak', __FILE__)
+  CodeLoader.run('kernel/Alpha')
 end
