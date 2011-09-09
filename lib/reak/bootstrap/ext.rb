721b191ee98ac33a3c6476f5f393fcba428d07b0
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-09-09 10:04:51 -0700

    move core extensions into modules

--- lib/reak/bootstrap/ext.rb
+++ lib/reak/bootstrap/ext.rb
@@ -1,8 +1,12 @@
-class Module
-  include Reak::Tools
-  extend Reak::Tools
+module Reak
+  module Ext
+    module Module
+      include Reak::Tools
+      append_features ::Module
 
-  def define_smalltalk_method(name, &block)
-    define_method(smalltalk_prefix(name), &block)
+      def define_smalltalk_method(name, &block)
+        define_method(smalltalk_prefix(name), &block)
+      end
+    end
   end
 end
