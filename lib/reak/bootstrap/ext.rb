f575fa11f626df54d7edde06640361e1259a2044
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-09-09 18:18:27 -0700

    add Class#subclass

--- lib/reak/bootstrap/ext.rb
+++ lib/reak/bootstrap/ext.rb
@@ -8,5 +8,16 @@ module Reak
         define_method(smalltalk_prefix(name), &block)
       end
     end
+
+    module Class
+      append_features ::Class
+
+      def subclass(name = nil, container = Object, &block)
+        subclass = ::Class.new(self)
+        container.const_set(name, subclass) if name
+        subclass.class_eval(&block) if block
+        subclass
+      end
+    end
   end
 end
